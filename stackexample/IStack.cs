29ea8b9f7a90d2da32d72832e46c43efe21e5350
noelplouzeau <noel.plouzeau@univ-rennes1.fr>
2024-04-03 14:51:58 +0200

    doc: add first XML doc comments

--- stackexample/IStack.cs
+++ stackexample/IStack.cs
@@ -1,19 +1,33 @@
-﻿using System;
-namespace stackexample
+﻿namespace stackexample
 {
-	public interface IStack<T>
-	{
-		T Top();
+    /// <summary>
+    /// A simple definition of a classical stack structure.
+    /// </summary>
+    /// <typeparam name="T">The type of the values in the stack</typeparam>
+    public interface IStack<T>
+    {
+        /// <summary>
+        /// Indicates if the stack contains values.
+        /// </summary>
+        /// <returns>true iff there are values inside</returns>
+        bool IsEmpty();
 
-		void Pop();
+        /// <summary>
+        /// Provides the number of values stored in the stack.
+        /// </summary>
+        /// <returns></returns>
+        int Height();
 
-		void Push(T value);
 
-		void Clear();
+        T Top();
 
-		int Height();
+        void Pop();
 
-		int Capacity();
-	}
+        void Push(T value);
+
+        void Clear();
+
+        int Capacity();
+    }
 }
 
---
+++ stackexampletest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using stackexample;
+
+namespace stackexampletest;
+
+public class Tests
+{
+    IStack<double> stack;
+    int capacity = 20;
+
+    [SetUp]
+    public void Setup()
+    {
+        stack = new stackexample.Stack<double>(capacity);
+    }
+
+    [Test]
+    public void EmptyAtCreation()
+    {
+        Assert.That(stack.Height(), Is.EqualTo(0));
+        Assert.Pass();
+    }
+}
