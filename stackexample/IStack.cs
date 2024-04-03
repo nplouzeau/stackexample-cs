cdac53bd9d7e212302667acf37b55ba131c3ab49
noelplouzeau <noel.plouzeau@univ-rennes1.fr>
2024-04-03 14:16:40 +0200

    Start of implementation (no tests)

---
+++ stackexample/IStack.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace stackexample
+{
+	public interface IStack
+	{
+	}
+}
+
---
+++ stackexample/Stack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace stackexample
+{
+	public class Stack<T> : IStack<T>
+	{
+        private T[] values;
+
+        private int top;
+
+
+		public Stack(int capacity)
+		{
+            values = new T[capacity];
+		}
+
+        public int Capacity()
+        {
+            return values.Length;
+        }
+
+        public void Clear()
+        {
+            top = -1;
+        }
+
+        public int Height()
+        {
+            return top + 1;
+        }
+
+        public void Pop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Push(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Top()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
