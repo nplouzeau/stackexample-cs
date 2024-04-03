66d6655f4d3da575973eb405dcd3d40a47e4c49c
noelplouzeau <noel.plouzeau@univ-rennes1.fr>
2024-04-03 14:58:17 +0200

    doc: add the remaining XML  documentation for the method of the IStack interface

--- stackexample/IStack.cs
+++ stackexample/IStack.cs
@@ -18,15 +18,33 @@
         /// <returns></returns>
         int Height();
 
-
+        /// <summary>
+        /// Provides the value at the top of the stack,
+        /// i.e. the last one that was pushed.
+        /// </summary>
+        /// <returns>top value of the stack</returns>
         T Top();
 
+        /// <summary>
+        /// Removes the top value of the stack.
+        /// </summary>
         void Pop();
 
+        /// <summary>
+        /// Adds a new value as the new top of the stack.
+        /// </summary>
+        /// <param name="value">the value to be add</param>
         void Push(T value);
 
+        /// <summary>
+        /// Removes of values in the stack, making it empty.
+        /// </summary>
         void Clear();
 
+        /// <summary>
+        /// Provides the maximum number of values that can be in the stack.
+        /// </summary>
+        /// <returns>the capacity of the stack</returns>
         int Capacity();
     }
 }
